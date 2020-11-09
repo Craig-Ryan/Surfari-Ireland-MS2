3989217c6d9a87ff7ac3f379fb3fb30b4ab0d0fc
Craig Ryan <craig@craigryan.org>
2020-11-09 16:33:42 +0000

    Added sticky navbar

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+function toggleNavigation() {
+    var navToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    navToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
