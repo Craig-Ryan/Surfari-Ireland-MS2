eedf34c1cf744edfee8a56b4bb4d9c34604e2e11
Craig Ryan <craig@craigryan.org>
2020-11-12 11:13:50 +0000

    Began re-styling footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//Start Navbar scroll JS 
+
 window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
@@ -10,3 +12,4 @@ function toggleNavigation() {
     navigation.classList.toggle('active')
 }
 
+//End Navbar scroll JS 
