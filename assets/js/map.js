fb464f081d40d7914fbb80f2248575e232bb2af2
Craig Ryan <craig@craigryan.org>
2020-11-16 15:48:41 +0000

    Began styling map section

---
+++ assets/js/map.js
@@ -0,0 +1,15 @@
+function initMap() {
+var map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 5,
+  center: {
+    lat: 54.498706,
+    lng: -8.265106,
+  }
+});
+
+
+}
+
+initMap();
+
+
--- assets/js/scriptToggle.js
+++ assets/js/scriptToggle.js
@@ -13,22 +13,3 @@ function toggleNavigation() {
 }
 
 //End Navbar scroll JS 
-function initMap() {
-var mapDonegal = new google.maps.Map(document.getElementById("map"), {
-  zoom: 5,
-  center: {
-    lat: 54.498706,
-    lng: -8.265106,
-  }
-});
-}
-
-function initMap2() {
-var mapSligo = new google.maps.Map(document.getElementById("map-2"), {
-  zoom: 5,
-  center: {
-    lat: 54.295404,
-    lng: -8.946930,
-  }
-});
-}
