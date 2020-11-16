b9a80e9efee4b92c6ca0f26b4492dbcc83e4a25d
Craig Ryan <craig@craigryan.org>
2020-11-16 22:15:35 +0000

    Added functionality to email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,11 @@
-console.log("hello");
+//Code inspired by Interactive Frontend Development course materials and personal contributions
+function sendMail(contactForm) {
+  emailjs.send("gmail", "surfari_ireland", {
+    from_name: contactForm.name.value,
+    from_email: contactForm.emailaddress.value,
+    message: contactForm.comments.value
+  })
+  .then(alert("Your message has been sent successfuly!"));
+      
+  $("#contact-form").text();
+}
