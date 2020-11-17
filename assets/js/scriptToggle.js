f2266dda1263ecf51396585dc6f3a9d4a00e1e53
Craig Ryan <craig@craigryan.org>
2020-11-17 21:54:38 +0000

    Worked on forecast section

--- assets/js/scriptToggle.js
+++ assets/js/scriptToggle.js
@@ -1,15 +1,2 @@
-//Start Navbar scroll JS 
 
-// window.addEventListener('scroll', function() {
-//     var header = document.querySelector('header');
-//     header.classList.toggle('sticky', window.scrollY > 0);
-// });
 
-// function toggleNavigation() {
-//     var navToggle = document.querySelector('.toggle');
-//     var navigation = document.querySelector('.navigation');
-//     navToggle.classList.toggle('active')
-//     navigation.classList.toggle('active')
-// }
-
-//End Navbar scroll JS 
