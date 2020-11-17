602f8e6255b90dc43543a89338e19ff255cf163a
Craig Ryan <craig@craigryan.org>
2020-11-17 13:40:22 +0000

    Began adding marker points to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,4 @@
+// Map code from 
 function initMap() {
 var map = new google.maps.Map(document.getElementById("map"), {
   zoom: 6,
@@ -7,9 +8,42 @@ var map = new google.maps.Map(document.getElementById("map"), {
   }
 });
 
+var markers = [
 
+  {
+    latlong: {lat: 54.498706, lng: -8.265278},
+    content: "<h5>Tullan Strand</h5><a href='index.html#bundoran'>Click here for more info</a>"
+  },
+
+  {
+    latlong: {lat: 54.481584, lng: -8271604},
+    content: "<h5>Bundoran Surf Co</h5><a href='index.html#bundoran'>Click here for more info</a>"
+  },
+
+];
+
+for (var i = 0; i <markers.length; i++) {
+  addMarker(markers[i]);
 }
 
-initMap();
+function addMarker(props) {
+  var marker = new google.maps.Marker( {
+    position: props.latlong,
+    map: map,
+  });
+
+  if (props.content) {
+    var infoWindow = new google.maps.InfoWindow( {
+      content: props.content,
+    });
+
+     marker.addListener("click", function () {
+        infoWindow.open(map, marker);
+        infowindow.close();
+      });
+  }
+}
+}
+
 
 
